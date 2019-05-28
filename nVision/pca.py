fc3b848c26e270b4351ae0c3a5d37b05b09518cd
Chad Curtis <ccurtis7@uw.edu>
2019-05-28 12:46:31 -0700

    Updated pca.py

--- nVision/pca.py
+++ nVision/pca.py
@@ -2,9 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def interaction_features(data):
 
-    return new_data
+    new_cols = []
+    new_data = pd.DataFrame([])
+    for i, xi in enumerate(data.columns.tolist()):
+
+        for xj in data.columns.tolist()[i:]:
+            new_cols.append(xi + ':' + xj)
+            new_data[xi + ':' + xj] = data[xi] * data[xj]
+            # print("Done with iteration {}".format(xi + ':' + xj))
+
+        comb_data = pd.concat([data, new_data], axis=1)
+
+    return comb_data
 
 
 def pca_analysis(data):
