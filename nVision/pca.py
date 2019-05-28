29353f972e0d2ce6b3326e2fa8c753f13170638c
Chad Curtis <ccurtis7@uw.edu>
2019-05-28 12:11:50 -0700

    Added skeleton .py file

---
+++ nVision/pca.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def interaction_features(data):
+
+    return new_data
+
+
+def pca_analysis(data):
+
+    return new_data, pca_model, components
