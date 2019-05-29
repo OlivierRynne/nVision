07b6f135f5b301f6f42dd51ba5106559610d4dd1
Qian Chen <qchen00923@gmail.com>
2019-05-29 10:50:10 -0500

    Added docstring for pca_analysis

--- nVision/pca.py
+++ nVision/pca.py
@@ -24,6 +24,40 @@ def interaction_features(data):
 
 
 def pca_analysis(data, n_components=None):
+    """
+    Perform the principal component analysis (PCA) to the input dataset 
+    to reduce the dimensionality and to project the data to a 
+    lower dimensional space.
+
+    Returns the projected data after PCA, the PCA model and the components.
+    The data are centered and scaled before PCA analysis.    
+
+    Parameters
+    ----------
+    data : pandas.core.frame.DataFrame
+        An m-by-n dataset with m observations and n features.
+    n_components : None or int, optional
+        The number of components. The default value is the number of features 
+        of the dataset. Must be the lesser value of the number of observations
+        and the number of features.
+
+    Returns
+    -------
+    new_data : pandas.core.frame.DataFrame, returns to the data after 
+    dimensionality reduction.
+    pca_model : sklearn.decomposition.pca.PCA, returns to all the options 
+    used in the PCA analysis.
+    components : pandas.core.frame.DataFrame, returns to the calculated 
+    principal axes in feature space, representing the directions of 
+    maximum variance in the data. 
+    
+    Examples
+    --------
+    >>> data = pd.DataFrame(np.array([[2,0],[0,2]]))
+    >>> new_data, pca_model, comps = pca.pca_analysis(data)
+    
+    
+    """
 
     scaler = preprocessing.StandardScaler().fit(data)
 
